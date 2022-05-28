83da0123bb05e298de77f5fa4e5f56f1854f3892
hyoomi <gbaleeheel@gmail.com>
2022-05-28 17:04:05 +0900

    Visualize accuracy and loss

--- cnn.py
+++ cnn.py
@@ -3,7 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import warnings
+import os
 warnings.filterwarnings('ignore')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 print("===== CNN =====")
 
@@ -22,22 +24,27 @@ x_train, x_valid, y_train, y_valid = train_test_split(x_load, y_load, test_size=
 # (1853, 100, 100, 3) / (206, 100, 100, 3)
 
 # Normalize data
-x_train = x_train / 255.0
-x_valid = x_valid / 255.0
+x_train = x_train.astype('float32') / 255.
+x_valid = x_valid.astype('float32') / 255.
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_valid.shape[0], 'test samples')
 
 # [Note]
 # 합성곱 연산: input data에 filter를 적용한 것
 # 패딩: 합성곱 연산 수행 전 데이터 주변을 특정값으로 채워 늘리는 것
-# stride: input data에 filter를 적용하는 위치의 간격격
+# stride: input data에 filter를 적용하는 위치의 간격
+
 # Create Model
 model = keras.Sequential()
-model.add(keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(100,100,3)))
+model.add(keras.layers.Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=(100, 100, 3)))
 model.add(keras.layers.MaxPooling2D(2, 2))
 model.add(keras.layers.Dropout(0.2))
 
-model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(keras.layers.Conv2D(64, kernel_size=(5, 5), activation='relu'))
 model.add(keras.layers.MaxPooling2D(2, 2))
-model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(keras.layers.Conv2D(64, kernel_size=(5, 5), activation='relu'))
 model.add(keras.layers.Dropout(0.2))
 
 model.add(keras.layers.Flatten())
@@ -46,7 +53,7 @@ model.add(keras.layers.Dense(10, activation='softmax'))
 
 # Model train
 model.summary()
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics='accuracy')
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 history = model.fit(x_train, y_train, epochs=10)
 
 # Evaluate model
@@ -54,10 +61,20 @@ loss, accuracy = model.evaluate(x_valid, y_valid)
 print("loss = ", loss)
 print("accuracy = ", accuracy)
 
-# Test
-test_batch = x_valid[:2]
-preds = model.predict(test_batch)
-print("preds = ", preds)
-print(np.argmax(preds[0]))
-print(np.argmax(preds[1]))
+# Visualize accuracy and loss
+plt.plot(history.history['accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
 
